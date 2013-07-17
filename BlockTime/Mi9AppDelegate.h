5ed6941f87fa70099e051e382b72ea172b85d0b5
Daphne Chong <daphne.h.chong@gmail.com>
2013-07-17 18:10:22 +1000

    Initial Commit

---
+++ BlockTime/Mi9AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  Mi9AppDelegate.h
+//  BlockTime
+//
+//  Created by Daphne Chong on 17/07/13.
+//  Copyright (c) 2013 Daphne Chong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class Mi9ViewController;
+
+@interface Mi9AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) Mi9ViewController *viewController;
+
+@end
---
+++ BlockTime/Mi9ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Mi9ViewController.h
+//  BlockTime
+//
+//  Created by Daphne Chong on 17/07/13.
+//  Copyright (c) 2013 Daphne Chong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Mi9ViewController : UIViewController
+
+@end
